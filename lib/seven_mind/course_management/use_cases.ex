28e2b8e472e508dc43f5ff0ff236fc9a712fcfcd
KingdomCoder <kingdomcoding@gmail.com>
2024-01-06 14:02:28 +0100

    Update use_cases.ex

--- lib/seven_mind/course_management/use_cases.ex
+++ lib/seven_mind/course_management/use_cases.ex
@@ -1,14 +1,11 @@
 defmodule SevenMind.CourseManagement.UseCases do
   alias SevenMind.Application
-  
+
   def create_category(category_name) do
     with {:ok, command} <- SevenMind.CourseManagement.Commands.CreateCategory.create(%{name: category_name}),
           :ok <- Application.dispatch(command)
     do
       {:ok, command}
-    else
-      {:error, reason} ->
-        {:error, reason}
     end
   end
 end
