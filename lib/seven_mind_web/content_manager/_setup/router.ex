eba7911a56205641693dcd604fd69c5413cefa31
KingdomCoder <kingdomcoding@gmail.com>
2024-01-06 12:44:11 +0100

    Open API specs modified

--- lib/seven_mind_web/content_manager/_setup/router.ex
+++ lib/seven_mind_web/content_manager/_setup/router.ex
@@ -5,5 +5,14 @@ defmodule SevenMindWeb.ContentManager.Setup.Router do
     # optionally a json_schema route
     json_schema: "/json_schema",
     # optionally an open_api route
-    open_api: "/open_api"
+    open_api: "/open_api",
+    modify_open_api: {__MODULE__, :modify_open_api, []}
+
+  def modify_open_api(spec, _, _) do
+    servers = Enum.map(spec.servers, fn server ->
+      %{server | url: "#{server.url}/api/content-manager"}
+    end)
+    
+    %{spec | servers: servers}
+  end
 end
