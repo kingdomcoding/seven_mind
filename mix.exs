77e5ada74b79a9dc003d6d3f50985fb3a9d3f713
KingdomCoder <kingdomcoding@gmail.com>
2024-01-08 10:21:05 +0100

    Tests moved to lib folder

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule SevenMind.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
+      test_paths: ["lib"],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
